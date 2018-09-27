5264236d7b197e5956165a84b2fe97f14dafe393
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-09-27 20:52:46 +0200

    Avogrado

---
+++ 04 - Variabelen/Avogadro.py
@@ -0,0 +1,12 @@
+# invoer
+nx = float(input('Geef aantal mol deeltjes: '))
+ms = 32.06
+na = 6.020 * 10**23
+
+# berekening
+resultaat1 = nx * ms
+resultaat2 = nx * na
+
+# uitvoer
+print(resultaat1)
+print(resultaat2)
