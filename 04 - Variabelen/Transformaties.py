e6dd7874e21ea307e82b07b17215c54f6ff13909
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-09-30 18:03:20 +0200

    Transformaties

---
+++ 04 - Variabelen/Transformaties.py
@@ -0,0 +1,15 @@
+# invoer
+a = float(input('Geef a: '))
+b = float(input('Geef b: '))
+
+# gegevens
+x = 'x'
+fx = '2(x - 3)^2 + 4'
+
+# berekening
+gx = '2' + '(x - ' + str(int(3 + a)) + ')^2' + ' + ' + str(int(4 + b))
+
+# uitvoer
+print('f(x) = ' + fx)
+resultaat = gx
+print('g(x) = '+ str(resultaat))
