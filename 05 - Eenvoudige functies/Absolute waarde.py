eeef262fb34796805491ddfc94e8e26ac564d914
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-10-04 10:00:21 +0200

    Wisselgeld

---
+++ 05 - Eenvoudige functies/Absolute waarde.py
@@ -0,0 +1,10 @@
+# invoer
+x = float(input('geef x: '))
+y = float(input('geef y: '))
+
+# bewerking
+linkerlid = abs(x) - abs(y)
+rechterlid = abs(x -4 y)
+
+# uitvoer
+print('{:.4f} ≤ {:.4f}'.format(linkerlid, rechterlid))
