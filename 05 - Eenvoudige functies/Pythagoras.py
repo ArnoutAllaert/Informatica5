f58f99c5e2c8a9583252c1332c5b3414f733068d
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-10-09 21:10:35 +0200

    Pythagoras

---
+++ 05 - Eenvoudige functies/Pythagoras.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+# invoer
+a = float(input('Geef a groter dan 0: '))
+b = float(input('Geef b groter dan 0: '))
+
+# bewerking
+c = sqrt((a ** 2) + (b ** 2))
+
+# uivoer
+print('In een rechthoekige driehoek met rechthoekszijden a = ' + str(round(a, 2)) + ' en b = ' + str(round(b, 2)) + ' is de schuine ' + str(round(c, 2)))
