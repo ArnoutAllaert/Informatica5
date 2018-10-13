aef5157701e1cb9e1afd451d4a9ab402f699dcb3
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-10-13 14:56:27 +0200

    risk

--- 06 - Condities/Risk.py
+++ 06 - Condities/Risk.py
@@ -10,9 +10,9 @@ if a >= b and a >= c:
     if a > d and a > e:
         if (b > d and b > e) or (c > d and c > e):
             uitkomst = 'aanvaller verliest 0 legers, verdediger verliest 2 legers'
-        if (b < d and b < e) or (c < d and c < e):
+        elif (b < d and b < e) or (c < d and c < e):
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if (b == d or b == e) or (c == d or c == e):
+        elif (b == d or b == e) or (c == d or c == e):
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
 
     if a < d and a < e:
@@ -20,54 +20,54 @@ if a >= b and a >= c:
     if a == d:
         if b > e or c > e:
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if b <= e or c <= e:
+        elif b <= e or c <= e:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
     if a == e:
         if b > d or c > d:
             uitkomst= 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if b <= d or c <= d:
+        elif b <= d or c <= d:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
 
 if b >= a and b >= c:
     if b > d and b > e:
         if (a > d and a > e) or (c > d and c > e):
             uitkomst = 'aanvaller verliest 0 legers, verdediger verliest 2 legers'
-        if (a < d and a < e) or (c < d and c < e):
+        elif (a < d and a < e) or (c < d and c < e):
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if (a == d or a == e) or (c == d or c == e):
+        elif (a == d or a == e) or (c == d or c == e):
                 uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
     if b < d and b < e:
         uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
     if b == d:
         if a > e or c > e:
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if a <= e or c <= e:
+        elif a <= e or c <= e:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
     if b == e:
         if a > d or c > d:
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if a <= d or c <= d:
+        elif a <= d or c <= d:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
 
 if c >= a and c >= b:
     if c > d and c > e:
         if (a > d and a > e) or (b > d and b > e):
             uitkomst = 'aanvaller verliest 0 legers, verdediger verliest 2 legers'
-        if (a < d and a < e) or (b < d and b < e):
+        elif (a < d and a < e) or (b < d and b < e):
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if (a == d or a == e) or (b == d or b == e):
+        elif (a == d or a == e) or (b == d or b == e):
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
     if c < d and c < e:
         uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
     if c == d:
         if a > e or b > e:
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if a <= e or b <= e:
+        elif a <= e or b <= e:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
     if c == e:
         if a > d or b > d:
             uitkomst = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
-        if a <= d or b <= d:
+        elif a <= d or b <= d:
             uitkomst = 'aanvaller verliest 2 legers, verdediger verliest 0 legers'
 
 # uitvoer
