46c14ffe77ff7e23f5d217b958dffcda3ab4f695
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-10-13 13:33:19 +0200

    blad, steen, schaar

---
+++ 06 - Condities/Blad-Steen-Schaar.py
@@ -0,0 +1,16 @@
+# invoer
+x = str(input('blad,stten,schaar: '))
+y = str(input('blad,stten,schaar: '))
+
+#bewerking
+if x == y:
+    uitkomst = 'onbeslist'
+
+if (x == 'blad' and y == 'steen') or (x == 'steen' and y == 'schaar') or (x == 'schaar' and y == 'blad'):
+    uitkomst = 'speler 1 wint'
+
+if (x == 'steen' and y == 'blad') or (x == 'schaar' and y == 'steen') or (x == 'blad' and y == 'schaar'):
+    uitkomst = 'speler 2 wint'
+
+# uitvoer
+print(uitkomst)
