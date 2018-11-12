1a60ed0b1ac22bb05021a623e515ac1a9f231fef
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-11-12 18:02:43 +0100

    grootste + gemiddelde

--- 07 - Iteraties - Forlus/Het grootste en het gemiddelde.py
+++ 07 - Iteraties - Forlus/Het grootste en het gemiddelde.py
@@ -1,16 +1,15 @@
 #invoer
 getallen= int(input('aantal getallen xi∈Z: '))
-max = 0
-nieuw_max = 0
+max = -87304958934789679237595637846586
 som = 0
 
 #bewerking
 for i in range(getallen):
     getal = int(input('getal: '))
-    if getal > max and getal > nieuw_max:
-        nieuw_max = getal
+    if getal > max:
+        max = getal
     som += getal
     gemiddelde = round((som / getallen), 2)
 
 #uitvoer
-print('{} {:d} {} {:.2f}'.format('Het grootste getal is ', nieuw_max ,' en het gemiddelde is ', gemiddelde))
+print('{} {:d} {} {:.2f}'.format('Het grootste getal is ', max ,' en het gemiddelde is ', gemiddelde))
