4184bf13192abd4ea3b65ce8da95ef141403d09a
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-11-20 17:59:39 +0100

    Priemgetallen

--- 07-Iteraties-Whilelus/Priemgetallen.py
+++ 07-Iteraties-Whilelus/Priemgetallen.py
@@ -1,5 +1,19 @@
 #invoer
 getal = int(input('Geef getal: '))
-
+x = 2
+y = 1
 
 #bewerking
+while x < getal:
+    if (getal % x) == 0:
+        uitkomst = str(getal) + ' is geen priemgetal'
+        y = 0
+    x += 1
+if y and getal != 1:
+    uitkomst = str(getal) + ' is een priemgetal'
+
+elif getal == 1:
+    uitkomst = '1 is geen priemgetal'
+
+#uitvoer
+print(uitkomst)
