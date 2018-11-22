245cf10c9073ca80244eb3c51b47da4b5c02bdb0
arnout969 <43027876+arnout969@users.noreply.github.com>
2018-11-22 08:58:29 +0100

    test: priemgetallen

--- 07-Iteraties-Whilelus/Test.py
+++ 07-Iteraties-Whilelus/Test.py
@@ -1,7 +1,13 @@
-getal = 0
-while getal < 5:
-    print(getal)
-    getal+= 1
+#priemgetallen
 
-for i in range(0,5):
-    print(i)
+getal = int(input('getal: '))
+#zolang je het niet kan delen door 2,3,4 is het allicht een priemgetal
+deler = 2
+
+while getal % deler != 0 and getal != 1:
+    deler += 1
+
+if deler == getal:
+    print('priemgetal')
+else:
+    print('geen priemgetal')
