244e60b3171f37a066b0696241e90d198ecef925
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-06 14:32:40 +0100

    Ave Caesar

--- 08-Functies/Ave Caesar.py
+++ 08-Functies/Ave Caesar.py
@@ -41,13 +41,13 @@ def versleutel(z, k):
 
             if is_letter(n) == True:
                 if n in 'abcdefghijklmnopqrstuvwxyz':
-                    if ord(n) + k >= 122:
+                    if ord(n) + k > 122:
                         return chr((ord(n) + k) - 26)
                     else:
                         return chr(ord(n) + k)
 
                 elif n in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-                    if ord(n) + k >= 90:
+                    if ord(n) + k > 90:
                         return chr((ord(n) + k) - 26)
                     else:
                         return chr(ord(n) + k)
