f1f32bceb64c4df57b1d1c174b83ea165055fa4a
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-06 14:19:53 +0100

    Ave Caesar

--- 08-Functies/Ave Caesar.py
+++ 08-Functies/Ave Caesar.py
@@ -31,32 +31,33 @@ def roteer_letter(n, k):
 
 
 def versleutel(n, k):
-    for letter in n:
-        def roteer_letter(letter, k):
-            def is_letter(letter):
+        def roteer_letter(n, k):
+            def is_letter(n):
                 if n in 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z' or n in 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z':
                     waarheid = True
                 else:
                     waarheid = False
                 return waarheid
 
-            if is_letter(letter) == True:
-                if letter in 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z':
-                    if ord(letter) + k >= 122:
-                        return chr((ord(letter) + k) - 26)
+            if is_letter(n) == True:
+                if n in 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z':
+                    if ord(ln) + k >= 122:
+                        return chr((ord(n) + k) - 26)
                     else:
-                        return chr(ord(letter) + k)
+                        return chr(ord(n) + k)
 
-                elif letter in 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z':
-                    if ord(letter) + k >= 90:
-                        return chr((ord(letter) + k) - 26)
+                elif n in 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z':
+                    if ord(n) + k >= 90:
+                        return chr((ord(n) + k) - 26)
                     else:
-                        return chr(ord(letter) + k)
-
+                        return chr(ord(n) + k)
             else:
-                return letter
-        return roteer_letter(letter, k)
+                return n
 
+        zin = ''
+        for letter in n:
+            zin += roteer_letter(n, k)
+        return zin
 
 print(versleutel('H', 20))
 print(versleutel('Het leven bestaat voor 10% uit dingen die gebeuren en voor 90% uit hoe jij daarop reageert.', 20))
