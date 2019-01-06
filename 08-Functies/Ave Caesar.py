30f0b5f6bec36d3b2fe9541fd90a4067e9066d2a
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-06 14:26:46 +0100

    Ave Caesar

--- 08-Functies/Ave Caesar.py
+++ 08-Functies/Ave Caesar.py
@@ -15,13 +15,13 @@ def roteer_letter(n, k):
         return waarheid
     if is_letter(n) == True:
         if n in 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z':
-            if ord(n) + k >= 122:
+            if ord(n) + k > 122:
                 return chr((ord(n) + k) - 26)
             else:
                 return chr(ord(n) + k)
 
         elif n in 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z':
-            if ord(n) + k >= 90:
+            if ord(n) + k > 90:
                 return chr((ord(n) + k) - 26)
             else:
                 return chr(ord(n)+k)
@@ -30,7 +30,7 @@ def roteer_letter(n, k):
         return n
 
 
-def versleutel(n, k):
+def versleutel(z, k):
         def roteer_letter(n, k):
             def is_letter(n):
                 if n in 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z' or n in 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z':
@@ -55,8 +55,8 @@ def versleutel(n, k):
                 return n
 
         zin = ''
-        for letter in n:
-            zin += roteer_letter(n, k)
+        for letter in z:
+            zin += roteer_letter(letter, k)
         return zin
 
 print(versleutel('H', 20))
