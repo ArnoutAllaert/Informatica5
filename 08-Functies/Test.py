00778dfcd1e2a48ee0e9092ef12ce9960e211c9d
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-08 20:55:18 +0100

    6174

--- 08-Functies/Test.py
+++ 08-Functies/Test.py
@@ -1,19 +1,3 @@
-def volgend_collatz_getal(n):
-    if n % 2 == 0:
-        volgend = n // 2
-    else:
-        volgend = (n * 3) + 1
-    return volgend
-
-
-def collatz(n):
-    collatz = 1
-    while volgend_collatz_getal(n) != 1 and n != 1:
-        collatz += 1
-        n = volgend_collatz_getal(n)
-    if n == 1:
-        collatz = 1
-    elif volgend_collatz_getal(n) == 1:
-        collatz += 1
-
-    return collatz
+b = (n - (a * 1000)) % 100
+c = (n - (a * 1000) - (b * 100)) % 10
+d = n - (a * 1000) - (b * 100) - (c * 10)
