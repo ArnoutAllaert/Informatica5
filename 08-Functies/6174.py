f108a998b3dbd98fdd19778b8c2f2de3b697a17a
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-10 09:05:51 +0100

    6174

--- 08-Functies/6174.py
+++ 08-Functies/6174.py
@@ -1,71 +1,51 @@
-def splits(n):
-    a = n // 1000
-    b = (n - (a * 1000)) // 100
-    c = (n - (a * 1000) - (b * 100)) // 10
-    d = n - (a * 1000) - (b * 100) - (c * 10)
-    cijfers = a, b, c, d
-    return cijfers
-
-def oplopende_cijfers(a,b,c,d):
-    g1 = min(a,b,c,d)
-    g2 = max(min(a,b,c),min(b,c,d),min(a,b,d),min(a,c,d))
-    g3 = min(max(a,b,c),max(b,c,d),max(a,b,d),max(a,c,d))
-    g4 = max(a,b,c,d)
-    return g1, g2, g3, g4
-
-def op_af_getallen(a,b,c,d):
-    oprij = str(a)+str(b)+str(c)+str(d)
-    afrij = str(d)+str(c)+str(b)+str(a)
-    return oprij, afrij
-
-def verschil(a,b):
-    verschil = str(int(a) - int(b))
-    return verschil
-
-def kaprekar(n):
-    mes =''
-    a = n // 1000
-    b = (n - (a * 1000)) // 100
-    c = (n - (a * 1000) - (b * 100)) // 10
-    d = n - (a * 1000) - (b * 100) - (c * 10)
-    g1 = min(a, b, c, d)
-    g2 = max(min(a, b, c), min(b, c, d), min(a, b, d), min(a, c, d))
-    g3 = min(max(a, b, c), max(b, c, d), max(a, b, d), max(a, c, d))
-    g4 = max(a, b, c, d)
-    oprij = str(g1) + str(g2) + str(g3) + str(g4)
-    afrij = str(g4) + str(g3) + str(g2) + str(g1)
-
-    if int(afrij) - int(oprij) == 6174:
-        mes = '{} - {} = {}'.format(afrij, oprij, int(afrij) - int(oprij))
-    else:
-
-        while int(afrij) - int(oprij) != 6174:
-            mes += '{} - {} = {}\n'.format(afrij, oprij, int(afrij) - int(oprij))
-            n = int(afrij) - int(oprij)
-            a = n // 1000
-            b = (n - (a * 1000)) // 100
-            c = (n - (a * 1000) - (b * 100)) // 10
-            d = n - (a * 1000) - (b * 100) - (c * 10)
-            g1 = min(a, b, c, d)
-            g2 = max(min(a, b, c), min(b, c, d), min(a, b, d), min(a, c, d))
-            g3 = min(max(a, b, c), max(b, c, d), max(a, b, d), max(a, c, d))
-            g4 = max(a, b, c, d)
-            oprij = str(g1) + str(g2) + str(g3) + str(g4)
-            afrij = str(g4) + str(g3) + str(g2) + str(g1)
-        a = n // 1000
-        b = (n - (a * 1000)) // 100
-        c = (n - (a * 1000) - (b * 100)) // 10
-        d = n - (a * 1000) - (b * 100) - (c * 10)
-        g1 = min(a, b, c, d)
-        g2 = max(min(a, b, c), min(b, c, d), min(a, b, d), min(a, c, d))
-        g3 = min(max(a, b, c), max(b, c, d), max(a, b, d), max(a, c, d))
-        g4 = max(a, b, c, d)
-        oprij = str(g1) + str(g2) + str(g3) + str(g4)
-        afrij = str(g4) + str(g3) + str(g2) + str(g1)
-        mes += '{} - {} = {}'.format(afrij, oprij, int(afrij) - int(oprij))
-    return mes
+def splits(getal):
+    d = getal % 10
+    getal = getal // 10
+    c = getal % 10
+    getal = getal // 10
+    b = getal % 10
+    getal = getal // 10
+    a = getal % 10
+    return a, b, c, d
 
 
+def oplopende_cijfers(a, b, c, d):
+    s1 = min(a, b, c, d)
+    s4 = max(a, b, c, d)
+    # middenste getallen bepalen
+    k23 = max(min(a, b), min(b, c), min(a, c))
+    k32 = a + b + c + d - s1 - s4 - k23
+    # volgorde middenste getallen
+    s2 = min(k23, k32)
+    s3 = max(k23, k32)
+    return s1, s2, s3, s4
 
 
-print(kaprekar(5432))
+def op_af_getallen(a, b, c, d):
+    return str(a) + str(b) + str(c) + str(d), str(d) + str(c) + str(b) + str(a)
+
+
+def verschil(af, op):
+    uitkomst = str(int(af) - int(op))
+    while len(uitkomst) < 4:
+        uitkomst = '0' + uitkomst
+    return uitkomst
+
+
+def kaprekar(getal):
+    uitkomst = ''
+    while getal != 6174:
+        a, b, c, d = splits(getal)
+        w, x, y, z = oplopende_cijfers(a, b, c, d)
+        op, af = op_af_getallen(w, x, y, z)
+        v = verschil(af, op)
+        uitkomst += af + ' - ' + op + ' = ' + v
+
+        getal = int(v)
+
+        if getal != 6174:
+            uitkomst += '\n'
+    return uitkomst
+
+
+print(kaprekar(5342))
