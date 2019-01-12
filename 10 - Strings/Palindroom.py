b8524e6924a42fc0870e634c24b66bd01f6dc6df
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-12 13:28:16 +0100

    palindroom

--- 10 - Strings/Palindroom.py
+++ 10 - Strings/Palindroom.py
@@ -1,14 +1,13 @@
 def is_palindroom(woord):
-    k = 0
+    mes = True
     if len(woord) == 1:
         mes = True
     else:
         for i in range(0, len(woord)//2):
-            if woord[i] == woord[len(woord) -1 - i] and k < 1000:
+            if woord[i] == woord[len(woord) -1 - i] and mes != False:
                 mes = True
             else:
                 mes = False
-                k += 1000
     return mes
 
 
