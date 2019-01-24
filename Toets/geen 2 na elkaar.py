d877d9b381a76cf16c984e1eca741050084e52b0
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-24 09:47:13 +0100

    cryptografie voor dummies

--- Toets/geen 2 na elkaar.py
+++ Toets/geen 2 na elkaar.py
@@ -1,8 +1,18 @@
 def ontdubbelen(woord):
+    nieuw_woord = woord
+    uitkomst = nieuw_woord
     for i in range(0, len(woord) - 1):
         if woord[i] == woord[i + 1]:
-            woord = woord[:i] + woord[i + 1]
+            nieuw_woord = woord[:i] + woord[i + 1:]
+    for i in range(0, len(nieuw_woord) - 1):
+        if nieuw_woord[i] == nieuw_woord[i + 1]:
+           uitkomst = nieuw_woord[:i] + nieuw_woord[i + 1:]
 
-    return woord
 
+    return uitkomst
+
+print(ontdubbelen('aaien'))
 print(ontdubbelen('stress'))
+print(ontdubbelen('grootte'))
+print(ontdubbelen('eieren'))
+
