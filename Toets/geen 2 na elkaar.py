e2ed2b2c14523b6520e2b23273465c2d8ff28a52
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-24 09:55:55 +0100

    geen 2 na elkaar

--- Toets/geen 2 na elkaar.py
+++ Toets/geen 2 na elkaar.py
@@ -1,18 +1,26 @@
+#def ontdubbelen(woord):
+#    nieuw_woord = woord
+#    uitkomst = nieuw_woord
+#    for i in range(0, len(woord) - 1):
+#        if woord[i] == woord[i + 1]:
+#            nieuw_woord = woord[:i] + woord[i + 1:]
+#    for i in range(0, len(nieuw_woord) - 1):
+#        if nieuw_woord[i] == nieuw_woord[i + 1]:
+#           uitkomst = nieuw_woord[:i] + nieuw_woord[i + 1:]
+
+
+#    return uitkomst
+
 def ontdubbelen(woord):
-    nieuw_woord = woord
-    uitkomst = nieuw_woord
-    for i in range(0, len(woord) - 1):
-        if woord[i] == woord[i + 1]:
-            nieuw_woord = woord[:i] + woord[i + 1:]
-    for i in range(0, len(nieuw_woord) - 1):
-        if nieuw_woord[i] == nieuw_woord[i + 1]:
-           uitkomst = nieuw_woord[:i] + nieuw_woord[i + 1:]
-
-
-    return uitkomst
+    nieuw_woord = woord[0]
+    for i in range(1, len(woord)):
+        if woord[i] != woord[i -1]:
+            nieuw_woord += woord[i]
+    return nieuw_woord
 
 print(ontdubbelen('aaien'))
 print(ontdubbelen('stress'))
 print(ontdubbelen('grootte'))
 print(ontdubbelen('eieren'))
+
 
