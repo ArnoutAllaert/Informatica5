666fc01820b21dd3b947dbe34d84cb8cc5dff4cc
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-01-24 09:12:31 +0100

    cryptografie voor dummies

---
+++ Toets/cryptografie voor dummies.py
@@ -0,0 +1,27 @@
+#Kleine letters worden hoofdletters;
+#Elk teken wordt vervangen door een teken dat n plaatsen (n<32) verder staat in de ASCII-tabel;
+#Indien het nieuwe teken een apenstaartje is, vervang dan het apenstaartje door een spatie;
+#Een spatie tussen twee woorden in de leesbare tekst wordt in de versleutelde tekst vervangen door een apenstaartje.
+
+def versleutel_woord(woord, n):
+    nieuw_woord = ''
+    uitkomst = ''
+    for letter in woord:
+        nieuw_woord += letter.upper()
+    for letter in nieuw_woord:
+        if chr(ord(letter) +n) == '@':
+            uitkomst += ' '
+        elif chr(ord(letter) +n) == ' ':
+            uitkomst += '@'
+        else:
+            uitkomst += chr(ord(letter) +n)
+    return uitkomst
+
+def versleutel_zin(zin, n):
+    nieuwe_zin = ''
+    for letter in zin:
+        if letter == ' ':
+            nieuwe_zin += '@'
+        else:
+            nieuwe_zin += versleutel_woord(letter, n)
+    return nieuwe_zin
