e430e55715edc23600ad222a00c447d1b77c4646
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-03-08 17:29:46 +0100

    mand

--- 13 - Dictionaries/Fruitmand.py
+++ 13 - Dictionaries/Fruitmand.py
@@ -9,8 +9,19 @@ def fruitmand_inpakken(mand):
     keze = []
     for v in mand.values():
         keze += [v]
-
-    return keze
+    kaas = [keze[0]]
+    for i in range(1, len(keze)):
+        if len(keze[i]) > len(kaas[len(kaas) - 1]):
+            kaas += [keze[i]]
+        elif len(keze[i]) < len(kaas[0]):
+            kaas = [keze[i]] + kaas
+        else:
+            a = 0
+            while a < len(kaas) -1:
+                if len(kaas[a]) < len(keze[i]) and len(keze[i]) < len(kaas[a + 1]):
+                    kaas = kaas[: a+1] + [keze[i]] + kaas[a+1:]
+                a += 1
+    return kaas
 
 print( fruitmand_maken(['banaan', 'aardbei', 'kiwi', 'peer', 'appel', 'bes', 'sinaasappel', 'framboos']))
 print(fruitmand_inpakken({6: 'banaan', 7: 'aardbei', 4: 'peer', 5: 'appel', 3: 'bes', 11: 'sinaasappel', 8: 'framboos'}))
