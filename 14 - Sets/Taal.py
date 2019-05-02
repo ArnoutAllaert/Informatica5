f32ac57ee891bd38131daa83dca4d2aea7fd0ccc
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-05-02 09:54:02 +0200

    vergeten woorden

--- 14 - Sets/Taal.py
+++ 14 - Sets/Taal.py
@@ -4,8 +4,11 @@ def behoort_tot_taal(woord, letters):
 
     return kaas.issubset(letters) and woord != ''
 
+def is_onleesbaar(woord, taal):
+    kaas = set(woord)
+    kaas.discard(' ')
+    heuj = kaas.intersection(taal)
+    return len(heuj) <= 1
 
-print(behoort_tot_taal('do well',{'o', 'd', 'e', 'l', 'w', 'r', 'h'}))
-print(behoort_tot_taal('ambigu',{'o', 'd', 'e', 'l', 'w', 'r', 'h'}))
-print(behoort_tot_taal('python',{'e', 'l', 'h', 'r', 'o', 'w', 'd'}))
-print(behoort_tot_taal('',{'e', 'l', 'h', 'r', 'o', 'w', 'd'}))
+print(is_onleesbaar('do well',{'o', 'd', 'e', 'l', 'w', 'r', 'h'}))
+print(is_onleesbaar('ambigu',{'o', 'd', 'e', 'l', 'w', 'r', 'h'}))
