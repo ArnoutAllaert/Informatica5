2bce06fb9c0aebc9a259a58488ef34fc8dbc60c1
arnout969 <43027876+arnout969@users.noreply.github.com>
2019-05-02 10:01:39 +0200

    vergeten woorden

--- 14 - Sets/Taal.py
+++ 14 - Sets/Taal.py
@@ -2,14 +2,14 @@ def behoort_tot_taal(woord, letters):
     kaas = set(woord)
     kaas.discard(' ')
 
-    return kaas.issubset(letters) and woord != ''
+    return kaas.issubset(letters) and len(kaas) > 0
 
 def is_onleesbaar(woord, taal):
     kaas = set(woord)
     kaas.discard(' ')
     heuj = kaas.intersection(taal)
 
-    return len(heuj) < 1
+    return len(heuj) == 0
 
 def perfect_woord(woord, taal):
     kaas = set(woord)
